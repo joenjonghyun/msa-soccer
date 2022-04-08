4e4f310fc08202751584b39c8071dc7cd4a39abb
joenjonghyun <jjh980814@naver.com>
2022-04-08 16:08:13 +0900

    22.04.08 16:00

--- soccer-next/pages/_app.js
+++ soccer-next/pages/_app.js
@@ -1,11 +1,9 @@
 import {Layout} from "common";
 
-function MyApp({ Component, pageProps}) {
+export default function MyApp({ Component, pageProps}) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   )
 }
-
-export default MyApp
--- soccer-next/pages/user/user-list.js
+++ soccer-next/pages/user/user-list.js
@@ -1,32 +1,45 @@
+import axios from "axios";
 import style from "common/style/table.module.css";
+import { useEffect, useState } from "react";
 
 const Table = ({ columns, colspan, data}) => {
     return(
     <table className={style.table}>
         <thead>
-        {/**<th key={column} className={style.td}>{column}</th> */}
+            {/**<th key={column} className={style.td}>{column}</th> */}
             <tr className={style.tr}>
-                {columns.map((column) => (
-                    <th key={column.username} className={style.td}>{column}</th>
-                ))}
+            {columns.map((column) => (
+                <th key = {column} className={style.td}>{column}</th>
+            ))}
         </tr>
         </thead>
         <tbody>
-            <tr className={style.tr}>
-                { data.length == 0 ? <td colSpan={colspan} className={style.td}>데이터가없습니다</td>
-                :<td colSpan={colspan} className={style.td}>데이터가있습니다</td>}
-            </tr>
+                { data.length == 0 ?<tr className={style.tr}>
+                                    <td colSpan={colspan} className={style.td}>데이터가없습니다</td>
+                                    </tr>
+                :data.map((user)=>(
+                <tr className={style.tr} key={user.username}>
+                    <td className={style.td}>{user.username}</td>
+                    <td className={style.td}>{user.password}</td>
+                    <td className={style.td}>{user.name}</td>
+                    <td className={style.td}>{user.telephone}</td>
+                    </tr>
+                ))}
+
         </tbody>
         </table>
     );
 }
 export default function UserList(){
     const columns = ["username", "password", "name", "telephone"];
-    const data = []
-    const count = data.length
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:5000/api/user/list').then(res=>{
+          setData(res.data.users)
+        }).catch(err=>{})
+      },[])
     return (<>
         <h1>회원목록</h1>
-        {count !=0 && <h3>회원수 : {count} 명</h3>}
         <div className={style.td}> 
         <Table columns={columns} colspan={4} data = {data}/>
         </div>
