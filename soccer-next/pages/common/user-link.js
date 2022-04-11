581456bf94b481bc81a443b92016dccd1436d87d
joenjonghyun <jjh980814@naver.com>
2022-04-11 18:05:26 +0900

    22.4.11

--- soccer-next/pages/common/user-link.js
+++ soccer-next/pages/common/user-link.js
@@ -6,8 +6,8 @@ export default function User(){
   return (
     <nav>
       <ul>
-        <li className={style.li}> <Link href='/user/signin'>로그인</Link> </li>
-        <li className={style.li}> <Link href='/user/signup'>로그아웃</Link> </li>
+        <li className={style.li}> <Link href='/user/login'>로그인</Link> </li>
+        <li className={style.li}> <Link href='/user/join'>회원가입</Link> </li>
         <li className={style.li}> <Link href='/user/user-list'>회원목록</Link> </li>
       </ul>
     </nav>
